8057a53b17673b514797125add416b257a9e3da2
Sahibzada Abubakar Bin Qasim <sahibzadaabubakar.b.qasim@jbhunt.com>
2023-03-11 00:40:22 -0600

    User security

--- src/main/java/com/example/demo/appuser/AppUser.java
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -6,6 +6,17 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 
 public class AppUser implements UserDetails {
+
+    // Defining Properties
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private AppUserRole appUserRole;
+    private Boolean locked;
+    private Boolean enabled;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
